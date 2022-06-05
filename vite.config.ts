039308d482e9a853c59b8b75352b43ce34c729ac
cyfung1031 <cyfung1031@gmail.com>
2022-06-05 21:29:15 +0800

    update files

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/Chinese-Conversion-WebApp/'
+  base: '/tcsc/',
+  build: {
+    outDir: 'docs'
+  }
 })
